171cb80babfbb9cb3139d598a01d06e2d574447c
lawrence nicastro <lawrence.nicastro1@gmail.com>
2022-12-25 22:37:33 -0500

    i cant figure out how to display a button

--- App.tsx
+++ App.tsx
@@ -1,26 +1,48 @@
-import 'react-native-url-polyfill/auto'
-import { useState, useEffect } from 'react'
-import { supabase } from './lib/supabase'
-import Auth from './components/Auth'
-import Account from './components/Account'
-import { View } from 'react-native'
-import { Session } from '@supabase/supabase-js'
+import "react-native-url-polyfill/auto";
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
+import Account from "./components/Account";
+import { View } from "react-native";
+import { Session } from "@supabase/supabase-js";
 import GeoLocation from "./components/GeoLocation";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+type RootStackParamList = {
+  auth: undefined;
+  account: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  const [session, setSession] = useState<Session | null>(null)
+  const [session, setSession] = useState<Session | null>(null);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
+      setSession(session);
+    });
 
     supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, [])
+      setSession(session);
+    });
+  }, []);
 
-  return <GeoLocation/>
+  return (
+    <NavigationContainer>
+      <Stack.Screen name="auth" component={() => <Auth />} />
+      {session ? (
+        <Stack.Screen
+          name="account"
+          component={() => <Account session={session} />}
+        />
+      ) : (
+        <Stack.Screen name="auth" component={() => <Auth />} />
+      )}
+      <GeoLocation />
+    </NavigationContainer>
+  );
 
   // return (
   //   <View>
--- components/GeoLocation.tsx
+++ components/GeoLocation.tsx
@@ -1,26 +1,72 @@
-import { View, StyleSheet, ScrollView, SafeAreaView, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  Text,
+  Pressable,
+} from "react-native";
 import Constants from "expo-constants";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-// import { GOOGLE_PLACES_API_KEY } from "@env";
-
-const GOOGLE_PLACES_API_KEY = "AIzaSyDDuch_D4BHz_2rv9Iz4L4bPJsqHMsMfWI";
+import {
+  GooglePlaceDetail,
+  GooglePlacesAutocomplete,
+} from "react-native-google-places-autocomplete";
+import {
+  GOOGLE_PLACES_API_KEY,
+  SUPABASE_ANON_KEY,
+  SUPABASE_EDGE_FUNCTION_GEO_LOCATION_URL,
+} from "@env";
+import axios from "axios";
+import { useState } from "react";
 
 export default function GeoLocation() {
-  console.log(GOOGLE_PLACES_API_KEY, "api");
+  const [lat, setLat] = useState<number | null>(null);
+  const [lng, setLng] = useState<number | null>(null);
+
+  const handleAddressPress = (details: GooglePlaceDetail | null) => {
+    if (details) {
+      const { lat, lng } = details?.geometry?.location;
+      setLat(lat);
+      setLng(lng);
+    }
+  };
+
+  const submitForReview = async () => {
+    const checkAddress = await axios.post(
+      SUPABASE_EDGE_FUNCTION_GEO_LOCATION_URL,
+      { lat, lng },
+      {
+        headers: {
+          Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+        },
+      }
+    );
+    console.log(checkAddress.data);
+  };
+
   return (
     <View style={{ paddingTop: Constants.statusBarHeight, flex: 1 }}>
+      <GooglePlacesAutocomplete
+        fetchDetails={true} // need this to fetch the details object
+        placeholder="Search"
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, '---', details);
+          handleAddressPress(details);
+        }}
+        query={{
+          key: GOOGLE_PLACES_API_KEY,
+          language: "en",
+        }}
+      />
 
-    <GooglePlacesAutocomplete
-      placeholder="Search"
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log(data, details);
-      }}
-      query={{
-        key: GOOGLE_PLACES_API_KEY,
-        language: "en",
-      }}
-    />
+      {/*<Pressable*/}
+      {/*  style={styles.button}*/}
+      {/*  // disabled={lat === null || lng === null}*/}
+      {/*  onPress={submitForReview}*/}
+      {/*>*/}
+      {/*  <Text>Submit for review</Text>*/}
+      {/*</Pressable>*/}
     </View>
   );
 }
@@ -29,8 +75,14 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
     flex: 1,
-    height: "100%",
+    height: "50%",
     padding: 10,
-    zIndex: 2,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 60,
+    backgroundColor: "blue",
   },
 });
